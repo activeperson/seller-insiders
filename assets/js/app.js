0cc9fc63d515f7d5e94da43b6a0329997307a7e6
Malyshko Vladislav <vladislavm@wizardsdev.com>
2020-05-02 19:30:51 +0300

    added wp

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,28 @@ $(function() {
 
  
 
+// $('.reviews__slider').slick({
+// 	 dots: false,
+// 	  infinite: false,
+// 	  speed: 800,
+// 	  slidesToShow: 1,
+// 	  slidesToScroll: 1,
+// 	  centerMode: true,
+// 	  adaptiveHeight: true,
+// 	  variableWidth: true,
+// 	  arrows: true,
+
+// 	  nextArrow: `<svg class="arrows-custom next" style="display: block; width: 17px; position: absolute; right: -40px; top: 40%;" viewBox="0 0 17.3 33" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <desc>Left</desc> <polyline fill="none" stroke="#222" stroke-linejoin="butt" stroke-linecap="butt" stroke-width="1" points="0.5,0.5 16.5,16.5 0.5,32.5"></polyline> </svg>`,
+// 	  prevArrow: `<svg class="arrows-custom prev" style="display: block; transform: rotate(180deg);  width: 17px; position: absolute; top: 40%; left: -40px;" viewBox="0 0 17.3 33" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <desc>Right</desc> <polyline fill="none" stroke="#222" stroke-linejoin="butt" stroke-linecap="butt" stroke-width="1" points="0.5,0.5 16.5,16.5 0.5,32.5"></polyline> </svg>`
+// });
 
 
 
 
+let burger = document.querySelector('.mobile-burger');
+burger.addEventListener('click', function() {
+	document.body.classList.toggle('active');
+});
 
   
 
